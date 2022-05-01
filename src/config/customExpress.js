fb0e5e11e2656c6aa979483114406b03eccd95c8
Mau-Me <mauriciosm95@gmail.com>
2022-05-01 02:33:44 -0300

    Update: app.use com urlencoded

--- src/config/customExpress.js
+++ src/config/customExpress.js
@@ -5,7 +5,7 @@ module.exports = () => {
   const app = express();
 
   app.use(express.json());
-  app.use(express.urlencoded());
+  app.use(express.urlencoded({ extended: true }));
 
   consign().include("src/controllers").into(app);
 
