42127b25c5632351cc7a24b3f6df83e5cb988f01
mau-me <mauriciosm95@gmail.com>
2023-07-13 02:08:37 -0300

    feat: update uri connect to mongodb atlas

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
+import functions from "firebase-functions";
 
-const MongoDB_URI = process.env.MONGODB_URI;
+const MongoDB_URI = functions.config().afps.mongodb_uri;
 
 mongoose.connect(MongoDB_URI);
 
 const db = mongoose.connection;
 
 export default db;
+
