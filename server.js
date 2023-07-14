c502138e7be1d814a7483161c84e7dd252725dc0
mau-me <mauriciosm95@gmail.com>
2023-07-14 20:37:22 -0300

    feat: reinicializa o projeto

--- server.js
+++ server.js
@@ -1,7 +1,1 @@
-import app from "./src/app.js";
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log(`O servidor está escutando em http://localhost:${port}`);
-});
+console.log("O servidor está escutando em http://localhost:${port}");
